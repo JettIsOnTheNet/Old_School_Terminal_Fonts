1dd0dc9f3fd96343e7f6076f8fa9ff80ca50f1b1
JettIsOnTheNet <jettisonthenet@gmail.com>
2024-02-15 17:53:41 -0500

    Fixed the link location for the individial ttf files and regenerated the readme.

--- generate_readme.py
+++ generate_readme.py
@@ -39,7 +39,7 @@ def generate_preview_images(patched_dir_str, previews_dir_str, ttf_files_lst):
 
         fb.save(os.path.join(previews_dir_str, ttf_file + '.png'))
 
-def generate_readme(previews_dir_str, ttf_files_lst, header_file_str, readme_file_str):
+def generate_readme(patched_dir_str, previews_dir_str, ttf_files_lst, header_file_str, readme_file_str):
 
     header_file = open(header_file_str, 'r')
     header_body_str = header_file.read()
@@ -48,7 +48,7 @@ def generate_readme(previews_dir_str, ttf_files_lst, header_file_str, readme_fil
     readme_body = ''
 
     for ttf_file in ttf_files_lst:
-        readme_body += '### [' + ttf_file +'](' + previews_dir_str + '/' + ttf_file + ')\n![' + ttf_file + '](' + previews_dir_str + '/' + ttf_file + '.png)\n'
+        readme_body += '### [' + ttf_file +'](' + patched_dir_str + '/' + ttf_file + ')\n![' + ttf_file + '](' + previews_dir_str + '/' + ttf_file + '.png)\n'
     
     readme_file = open(readme_file_str, 'w')
     readme_file.write(header_body_str + readme_body)
@@ -56,5 +56,5 @@ def generate_readme(previews_dir_str, ttf_files_lst, header_file_str, readme_fil
 if __name__ == '__main__':
     ttf_files_lst = [f for f in os.listdir(patched_dir_str) if f.endswith('.ttf')]
     generate_preview_images(patched_dir_str, previews_dir_str, ttf_files_lst)
-    generate_readme(previews_dir_str, ttf_files_lst, header_file_str, readme_file_str)
+    generate_readme(patched_dir_str, previews_dir_str, ttf_files_lst, header_file_str, readme_file_str)
 
